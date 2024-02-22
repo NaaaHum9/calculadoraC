b798c24e1b2c4182ce4fc6ca1fe206277a1e725e
Nahum Velazquez <vgjn98x@gmail.com>
2024-02-21 22:29:53 -0600

    'FuncionSuma'

--- Calculadora.c
+++ Calculadora.c
@@ -1,6 +1,23 @@
 #include "stdio.h"
 
+//Prototipos
+int suma(int num1, int num2);
+
 int main(){
-    //Hola como estan?
-    return 0;
+    int num1, num2;
+    int res;
+
+    printf("\n ---Calculadora--- \n");
+    printf("Ingrese el primer numero: \n");
+    scanf("%i", &num1);
+    printf("Ingrese el segundo numero: \n");
+    scanf("%i", &num2);
+    
+    //funciones
+    res = suma(num1, num2);
+    printf("Resultado suma: %i", res);
+}
+
+int suma(int num1, int num2){
+    return num1 + num2;
 }
